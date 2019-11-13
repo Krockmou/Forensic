902f9537ae6c213f892270876ce059edc7ec2151
Krockmou <53081744+Krockmou@users.noreply.github.com>
2019-11-13 22:32:21 +0100

    Update envir_forensic.py

--- envir_forensic.py
+++ envir_forensic.py
@@ -1,18 +1,47 @@
 #coding:utf-8
 
 import os
-
+#Création du user case
 case = input ("Case name: ")
+#Test présence dossier Forensic
+here = os.path.exists("/root/Forensic")
 
-#Création de l'arborescence
-os.mkdir ("/root/Forensic")
-os.mkdir ("/root/Forensic/"+case)
-os.mkdir ("/root/Forensic/"+case+"/artefacts")
-os.mkdir ("/root/Forensic/"+case+"/source")
-os.mkdir ("/root/Forensic/"+case+"/browser")
-os.mkdir ("/root/Forensic/"+case+"/browser/chrome")
-os.mkdir ("/root/Forensic/"+case+"/browser/firefox")
-os.mkdir ("/root/Forensic/"+case+"/download")
-os.mkdir ("/root/Forensic/"+case+"/evidence")
-os.mkdir ("/root/Forensic/"+case+"/prefetch")
-os.mkdir ("/root/Forensic/"+case+"/recycle")
+if here :
+    print ("Le dossier Forensic existe déjà. Souhaitez vous ajouter votre cas dedans ? yes/no")
+    answer = input (">")
+    answer = str(answer)
+    if answer == "yes" :
+        os.mkdir ("/root/Forensic/"+case)
+        os.mkdir ("/root/Forensic/"+case+"/artefacts")
+        os.mkdir ("/root/Forensic/"+case+"/source")
+        os.mkdir ("/root/Forensic/"+case+"/browser")
+        os.mkdir ("/root/Forensic/"+case+"/browser/chrome")
+        os.mkdir ("/root/Forensic/"+case+"/browser/firefox")
+        os.mkdir ("/root/Forensic/"+case+"/download")
+        os.mkdir ("/root/Forensic/"+case+"/evidence")
+        os.mkdir ("/root/Forensic/"+case+"/prefetch")
+        os.mkdir ("/root/Forensic/"+case+"/recycle")
+        print ("Le case : {} a bien été créé dans le dossier Forensic.".format (case))
+
+    else :
+        print ("Au revoir bobo")
+
+else :
+    print ("Le dossier Forensic n'existe pas. Souhaitez vous le créér ? yes/no")
+    answer2 = input (">")
+    answer2 = str(answer2)
+    if answer2 == "yes" :
+        os.mkdir ("/root/Forensic")
+        os.mkdir ("/root/Forensic/"+case)
+        os.mkdir ("/root/Forensic/"+case+"/artefacts")
+        os.mkdir ("/root/Forensic/"+case+"/source")
+        os.mkdir ("/root/Forensic/"+case+"/browser")
+        os.mkdir ("/root/Forensic/"+case+"/browser/chrome")
+        os.mkdir ("/root/Forensic/"+case+"/browser/firefox")
+        os.mkdir ("/root/Forensic/"+case+"/download")
+        os.mkdir ("/root/Forensic/"+case+"/evidence")
+        os.mkdir ("/root/Forensic/"+case+"/prefetch")
+        os.mkdir ("/root/Forensic/"+case+"/recycle")
+        print ("Tous les dossier ont bien été créé dans le dossier Forensic, case : {}".format (case))
+    else :
+        print ("Au revoir")
